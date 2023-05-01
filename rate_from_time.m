5c7c43de8c610bd4a7971a0c53dc186f9d721888
Yiming Xu <yxu148@syr.edu>
2023-05-01 16:11:36 -0400

    fix a small bug
    Make the turn rate calculation right for all time

--- rate_from_time.m
+++ rate_from_time.m
@@ -1,6 +1,7 @@
 % Transferring from the time series in period of some incidents happen, t, to the
 % rate of it happening r.
 %[0, T] is the possible period the incident happens
+% s stepsize, b binsize
 
 function r = rate_from_time(t, T, s, b)
 
@@ -8,9 +9,13 @@ m = fix(T/s);
 r = zeros(1, m + 1);
 
 for j = 0 : m
-    tleft = mod(j*s, T);  % periodic boundary condition
-    tright = mod(j*s + b, T);
-    r(j+1) = nnz(t >= tleft & t < tright ) / b;
+    tleft = mod(j*s-b, T);  % periodic boundary condition
+    tright = mod(j*s, T);
+    if tleft>tright
+        r(j+1) = nnz(t >= tleft | t < tright ) / b;
+    else
+        r(j+1) = nnz(t >= tleft & t < tright ) / b;
+    end
 end
 
 end
