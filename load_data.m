29d0da94c13187d711399757838e074463cc4b7e
Yiming Xu <yxu148@syr.edu>
2024-02-15 11:31:43 -0500

    Optimize the way to get ledVals
    Acquire the ledVals from GlobalQuantity instead of gatherField of all tracks

--- load_data.m
+++ load_data.m
@@ -1,10 +1,10 @@
-basedir = 'G:\AS-Filer\PHY\mmihovil\Shared\Yiming Xu\data\variability_new_extracted\Gr21a@Chrimson(3)\T_Re_Sq_318to532P_20_1_3#T_Bl_Sq_2,5to6P_20_2_3';  % ---------------------------
+basedir = 'G:\AS-Filer\PHY\mmihovil\Shared\Yiming Xu\data\variability_new_extracted\Gr21a@Chrimson(3)\T_Re_Sq_219to436P_15_2_3#T_Bl_Sq_2to7P_15_1_3';  % ---------------------------
 d = dir(fullfile(basedir, 'matfiles', '*.mat'));
 % reload experiment from mat files, called experiment set (eset), belong to @ExperimentSet object
 disp('Loading data...');
-x = [14];  % load the x-th set of data to analyze, x is a list [1], or [1, 2, 5], or delete (x) below for all--------------------
+x = [3];  % load the x-th set of data to analyze, x is a list [1], or [1, 2, 5], or delete (x) below for all--------------------
 eset = ExperimentSet.fromMatFiles(fullfile(basedir, 'matfiles', {d(x).name}));  % d(x) or d
-pause('on');  % 'on'---ask the user to press any key to save the figure, and continue; 'off'--directly save without asking
+pause('off');  % 'on'---ask the user to press any key to save the figure, and continue; 'off'--directly save without asking
 
 
 % load .mat files containing track information into eset
@@ -26,54 +26,47 @@ v_mean = 60 * mean([eset.expt.track.getSubFieldDQ('run', 'speed', 'position', 'm
 disp(['Mean run speed of this group is ', num2str(v_mean), ' cm/min'])
 mkdir(fullfile(basedir, ['results', d(x).name(end-16:end-4)]));  % auto name after date of expt, e.g. results_202309141250
 
-% eset.expt.globalQuantity.fieldname  % to check what field is
-
- led1Val = eset.gatherField('led1Val');  % Red Light intensity of LED at each frame
-% plot(eset.expt.elapsedTime(1:15e3), led1Val(1:15e3)); xlabel('Time (s)'); ylabel('led1Val');
-% savename = strcat(basedir,['\results', d(x).name(end-16:end-4)], '\led1Val_time');
-% savefig(gcf,savename);
-
-led2Val = eset.gatherField('led2Val');  % Blue Light intensity (PWM) of LED at each frame
-% eset.expt goes to @Experiment object; waveform: square.
+% eset.expt.globalQuantity.fieldname to check what field is, the field doesn't depend on which track
+field_name = {eset.expt.globalQuantity.fieldname};
+GQled1Val = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led1Val'}));
+GQled2Val = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led2Val'}));
 
 % create a new globalquantity which is square wave across the whole experiment time
-inds_led1Val = find(strcmpi('led1Val', {eset.expt.globalQuantity.fieldname}));
-inds_led2Val = find(strcmpi('led2Val', {eset.expt.globalQuantity.fieldname}));
-xdata = eset.expt(1).globalQuantity(inds_led1Val).xData; 
-ydata = eset.expt(1).globalQuantity(inds_led1Val).yData + eset.expt(1).globalQuantity(inds_led2Val).yData;
 ydata(12e3:24e3) = ydata(12e3:24e3) + 100;  % this is to make the square wave fluctruate around a center quantity
-eset.expt(1).addGlobalQuantity('eti', 'led12Val', xdata, ydata)
-led12Val = eset.gatherField('led12Val');
-
+xdata = GQled2Val.xData;  % xdata of all Global Quantity ledVals should be the same
+ydata = GQled1Val.yData + GQled2Val.yData;  % LED intensity in PWM
+eset.expt(1).addGlobalQuantity('eti', 'led12Val', xdata, ydata);  % create a man-made global field to add ton/toff
 eset.expt(1).addTonToff('led12Val', 'square');  % create time on/off field based a global quantity fieldname 'led2Val'
-ton = eset.gatherField('led12Val_ton');  % get all values of 'led2Val_on' for all track in ExperimentSet eset
-% return a k-N array of values, where N is total number of points, k is the dimension of the values of fieldname 'led2Val_off'
-toff= eset.gatherField('led12Val_toff'); 
+GQton = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led12Val_ton'}));
+GQtoff = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led12Val_toff'}));
+
 % correspond frame number to time, seems like the stimulation happens at 10 s of the 20 s period
-figure; t_end = 24e3-10;
-plot(eset.expt(1).elapsedTime(1 : t_end)/60, led1Val(1:t_end), 'r'); hold on;
-plot(eset.expt(1).elapsedTime(1 : t_end)/60, led2Val(1:t_end), 'b'); hold on;
-plot(eset.expt(1).elapsedTime(1:t_end)/60, toff(1:t_end), 'k'); hold on;
-plot(eset.expt(1).elapsedTime(1:t_end)/60, toff(1:t_end)+100, 'k'); hold off;
-xlabel('Time (min)'); legend('led1Val','led2Val', 'toff', 'toff');
+figure;
+plot(GQled2Val.xData/60, GQled1Val.yData, 'r'); hold on;
+plot(GQled2Val.xData/60, GQled2Val.yData, 'b'); hold on;
+plot(GQled2Val.xData/60, GQtoff.yData, 'k'); hold on;
+plot(GQled2Val.xData/60, GQtoff.yData+60, 'k'); hold off;
+xlabel('Time (min)'); legend('led1Val','led2Val', 'toff');
 pause;
 savename = strcat(basedir,['\results', d(x).name(end-16:end-4)], '\led12Val_toff');
 savefig(gcf,savename);
-close;
-% plot(eset.expt.elapsedTime(1:5e3),toff(1:5000), 'b'); xlabel('Time (s)'); hold off;  
-
+% close;
 
 tperiod = 20;  % depend on the name of .mat file loaded, '_18_', or from the plot led2Val-Time
 disp(['Time for one frame is ', num2str(eset.expt.elapsedTime(2)), ' s']);
 Ntracks = size(eset.expt(1).track);  % 1-by-Number_of_Tracks ( number of maggots)
 figure;
-histogram(round(eset.expt.elapsedTime([eset.expt.track.npts])/tperiod), [0:10:60]);  % round 60.001 to 60
+histogram(round(eset.expt.elapsedTime([eset.expt.track.npts])/tperiod), 0:10:120);  % round 60.001 to 60
 xlabel('Number of Periods'); ylabel('Number of Tracks'); title(['Histogram of The Length of All ', num2str(length(eset.expt(1).track)), ' Tracks']);
 pause;
 savename = strcat(basedir,['\results', d(x).name(end-16:end-4)], '\track_length');
 savefig(gcf,savename);
 close;
 
+% paramerters when generating BIN files of stimulation --------------------
+t_stim_start = [0, 600, 1200];  % start time (s) of each intensity of stimulation
+t_stim_end = [600, 1200, 1800];
+frame_rate = 20;  % number of frames per second
 
 nperiods = 69;  % select tracks that have [nperiods, Np eriods] length
 Nperiods = 91;  %expt time is 20 min, i.e. 20s periods at most for a 60 cycles, use 61 to include 60.001
