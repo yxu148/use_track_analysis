0385c8d325fc7afdea1a998c2bdd9f2f30c27783
Yiming Xu <yxu148@syr.edu>
2024-04-29 17:09:22 -0400

    Update load_data.m
    add function of copying itself to the results folder for back up

--- load_data.m
+++ load_data.m
@@ -1,11 +1,11 @@
-basedir = 'G:\AS-Filer\PHY\mmihovil\Shared\Yiming Xu\data\variability_new_extracted\Gr21a@Chrimson(3)\T_Re_Sq_219to436P_15_2_3#T_Bl_Sq_2to7P_15_1_3';  % ---------------------------
+basedir = 'G:\AS-Filer\PHY\mmihovil\Shared\Yiming Xu\data\variability_new_extracted\Gr21a@Chrimson(3)\T_Re_Sq_219to436P_15_2_3_5_6#T_Bl_Sq_2to7P_15_1_3_4_6';  % ---------------------------
 d = dir(fullfile(basedir, 'matfiles', '*.mat'));
 % reload experiment from mat files, called experiment set (eset), belong to @ExperimentSet object
 disp('Loading data...');
 x = [1];  % load the x-th set of data to analyze, x is a list [1], or [1, 2, 5], or delete (x) below for all--------------------
 eset = ExperimentSet.fromMatFiles(fullfile(basedir, 'matfiles', {d(x).name}));  % d(x) or d
 pause('off');  % 'on'---ask the user to press any key to save the figure, and continue; 'off'--directly save without asking
-
+tperiod = 15;  % depend on the name of .mat file loaded, '_18_', or from the plot led2Val-Time-----------------
 
 % load .mat files containing track information into eset
 disp('Loading tracks ...');
@@ -31,33 +31,48 @@ field_name = {eset.expt.globalQuantity.fieldname};
 GQled1Val = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led1Val'}));
 GQled2Val = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led2Val'}));
 
+% figure; plot(GQled1Val.xData, GQled1Val.yData, 'r', GQled2Val.xData, GQled2Val.yData, 'b');
+
 % create a new globalquantity which is square wave across the whole experiment time
 xdata = GQled2Val.xData;  % xdata of all Global Quantity ledVals should be the same
 ydata = GQled1Val.yData + GQled2Val.yData;  % LED intensity in PWM
-index = find(GQled1Val.yData==0, 1, 'last');  % find the index of the last zero element of led1Val, this index may be different from the initial setting because of hardware noise
-ydata(1:index) = ydata(1:index) + 60;  % this is to make the square wave fluctruate around a center quantity, use index to make the combined square wave cleaner
+index1 = find(GQled2Val.yData==15, 1, 'first'); % find the index of the first 15 element of led1Val
+temp_led1Val_yData = GQled1Val.yData(1:8e3);  % only select the first part so it's easier to pick index2
+index2 = find( temp_led1Val_yData==0, 1, 'last'); % find the index of the last zero element of led1Val, this index may be different from the initial setting because of hardware noise
+ydata(index1 : index2) = ydata(index1 : index2) + 60;  % this is to make the square wave fluctruate around a center quantity, use index to make the combined square wave cleaner
+temp_led1Val_yData = GQled1Val.yData(8e3:end);  % only select the last part so it's easier to find index3
+index3 = find(temp_led1Val_yData == 0, 1, 'first') + 8e3 - 1;  % the begining of dark red
+index4 = find(temp_led1Val_yData == 0, 1, 'last') + 8e3 - 1;  % the end of dark red
+ydata(index3 : index4) = ydata(index3 : index4) + 60;
+
+
 eset.expt(1).addGlobalQuantity('eti', 'led12Val', xdata, ydata);  % create a man-made global field to add ton/toff
-eset.expt(1).addTonToff('led12Val', 'square');  % create time on/off field based a global quantity fieldname 'led2Val'
+% eset.expt(1).addTonToff('led12Val', 'square', 'asymmetryOK', true);  % create time on/off field based a global quantity fieldname 'led2Val'
+eset.expt(1).addTonToff('led12Val', 'square', 'period', tperiod);  % create time on/off field based a global quantity fieldname 'led2Val'
 GQton = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led12Val_ton'}));
 GQtoff = eset.expt.globalQuantity(strcmp({eset.expt.globalQuantity.fieldname}, {'led12Val_toff'}));
+
+% figure; plot(xdata, ydata);
+% hold on;
+% plot(GQtoff.xData, GQtoff.yData + 60);
+% hold off;
 
 % correspond frame number to time, seems like the stimulation happens at 10 s of the 20 s period
 figure;
 plot(GQled2Val.xData/60, GQled1Val.yData, 'r'); hold on;
-plot(GQled2Val.xData/60, GQled2Val.yData, 'b'); hold on;
-plot(GQled2Val.xData/60, GQtoff.yData, 'k'); hold on;
-plot(GQled2Val.xData/60, GQtoff.yData+60, 'k'); hold off;
+plot(GQled2Val.xData/60, GQled2Val.yData, 'b');
+plot(GQled2Val.xData/60, GQtoff.yData, 'k');
+plot(GQled2Val.xData/60, GQtoff.yData + 60, 'k'); hold off;
 xlabel('Time (min)'); legend('led1Val','led2Val', 'toff');
 pause;
 savename = strcat(basedir,['\results', d(x).name(end-16:end-4)], '\led12Val_toff');
 savefig(gcf,savename);
 % close;
 
-tperiod = 15;  % depend on the name of .mat file loaded, '_18_', or from the plot led2Val-Time-----------------
 disp(['Time for one frame is ', num2str(eset.expt.elapsedTime(2)), ' s']);
 Ntracks = size(eset.expt(1).track);  % 1-by-Number_of_Tracks ( number of maggots)
 figure;
-histogram(round(eset.expt.elapsedTime([eset.expt.track.npts])/tperiod), 0:10:120);  % round 60.001 to 60
+histogram(round(eset.expt.elapsedTime([eset.expt.track.npts])/tperiod), 0:10:121);  % round 60.001 to 60
 xlabel('Number of Periods'); ylabel('Number of Tracks'); title(['Histogram of The Length of All ', num2str(length(eset.expt(1).track)), ' Tracks']);
 pause;
 savename = strcat(basedir,['\results', d(x).name(end-16:end-4)], '\track_length');
@@ -65,13 +80,13 @@ savefig(gcf,savename);
 % close;
 
 % paramerters when generating BIN files of stimulation --------------------
-t_stim_start = [0, 600, 1200];  % start time (s) of each intensity of stimulation
-t_stim_end = [600, 1200, 1800];
-stim_color = {'blue', 'red', 'bluered'};  % descripiton of the t_stim_start
+t_stim_start = [0, 300, 600, 900, 1200, 1500];  % start time (s) of each intensity of stimulation
+t_stim_end = [300, 600, 900, 1200, 1500, 1800];
+stim_color = {'blue1', 'red1', 'bluered1', 'blue2', 'red2', 'bluered2'};  % descripiton of the t_stim_start
 frame_rate = 20;  % number of frames per second
 
 % To save longer tracks in t
-nperiods = 79;  % select tracks that have [nperiods, Np eriods] length
+nperiods = 100;  % select tracks that have [nperiods, Np eriods] length
 Nperiods = 121;  %expt time is 20 min, i.e. 20s periods at most for a 60 cycles, use 61 to include 60.001
 disp(['After filtering out tracks within [', num2str(nperiods), ', ', num2str(Nperiods), '] periods']);
 t = eset.expt.track;
@@ -106,3 +121,8 @@ figure; plot(ntracks_frame);
 xlabel('Frame number'); ylabel('Number of recognized maggots');
 savename = strcat(basedir,['\results', d(x).name(end-16:end-4)], '\num_maggots_recognized');
 savefig(gcf, savename); 
+
+
+% copy this file to the results folder
+savename = strcat(basedir,['\results', d(x).name(end-16:end-4)]);  % folder location to save file
+copyfile('load_data.m', savename)  % copy the file to the location
