50ca8d48ba17e5292bf524684daeddd21c714927
Yiming Xu <yxu148@syr.edu>
2025-06-12 14:51:08 -0400

    add new func
    add rescale fig and save figs into PDF

--- editFig.m
+++ editFig.m
@@ -37,3 +37,33 @@ fig.Children(1).String = '';  % delete the big title
 savefig(fig, fullfile(figlocation, 'p_turn_no_pause_all_cshl2023.fig'));
 
 
+% save multiple figs into PDF
+basedir = 'G:\AS-Filer\PHY\mmihovil\Shared\Yiming Xu\data\variability_new_extracted\Or42a@Chrimson(3)\T_Re_Sq_219to436P_15_2_3_#T_Bl_Sq_2to7P_15_1_3_';
+d = dir(fullfile(basedir, 'results_2025*'));
+filename = fullfile(basedir, 'results', 'vRun_combined.pdf');
+for k = 1 : length(d)  % k-th experiment
+    figlocation = fullfile(d(k).folder, d(k).name, 'vRun_ton_individual.fig');
+    fig = openfig(figlocation);
+    exportgraphics(fig, filename, 'Append', true);
+    close(fig);
+end
+
+
+% Edit the copy of the figure saved in figlocation
+figlocation = 'G:\AS-Filer\PHY\mmihovil\Shared\Yiming Xu\data\variability_new_extracted\A27h@Chrimson(3)\T_Bl_Sq_5to125P_20_every_5P\results';
+copyfile(fullfile(figlocation, 'speed_mean_t_allExpt.fig'), fullfile(figlocation, 'speed_mean_t_allExpt_rescale.fig'));  % source, destination
+fig = openfig(fullfile(figlocation, 'speed_mean_t_allExpt_rescale.fig'));  % class of Figure
+ax = gca;  % current axes (with two y‐axes internally)
+hLeftLines = findobj(ax, 'Type','line', 'YAxis','left');  % find lines attached to the LEFT y‐axis
+a = 0.6;  b =  2.6;   % source limits
+c =  0;  d =  2.5;   % desired limits
+% remap just those curves
+for k = 1:numel(hLeftLines)
+    y = hLeftLines(k).YData;
+    y2 = (y - a) / (b - a) * (d - c) + c;
+    hLeftLines(k).YData = y2;
+end
+% now fix the left axis display only:
+yyaxis left
+ylim([c d]);
+savefig(fig, fullfile(figlocation, 'speed_mean_t_allExpt_rescale.fig'));
